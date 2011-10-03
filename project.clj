59ce12d296a03b01e67b6c025d15e0af318aec2d
Michael Neale <michael.neale@gmail.com>
2011-10-02 17:07:38 -0700

    Documentation improvements, and lein push enabled

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject lein-cloudbees "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Cloudbees lein deployment plugin for deploying your special snowflake apps really really easily. See github.com/michaelneale/lein-cloudbees for docs and installation instructions."
   :dependencies [[org.clojure/clojure "1.2.1"]
   					[com.cloudbees/cloudbees-api-client "1.0.0-SNAPSHOT"]
                     [lein-ring "0.4.5"]]	
+
+
+	:dev-dependencies [[lein-clojars "0.6.0"]]
                     )
