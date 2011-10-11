fe527fcd7234444ffc7a4ae2ab9040fa898e98db
Michael Neale <michael.neale@gmail.com>
2011-10-11 18:32:56 +1100

    Doco change

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject lein-cloudbees "1.0.0-SNAPSHOT"
-  :description "Cloudbees lein deployment plugin for deploying your special snowflake apps really really easily. See github.com/michaelneale/lein-cloudbees for docs and installation instructions."
+  :description "Cloudbees lein deployment plugin for deploying your special snowflake apps really really easily. See https://github.com/michaelneale/lein-cloudbees for docs and installation instructions. (when installed type lein cloudbees to see what you can do. For any ring based webapps."
   :dependencies [[org.clojure/clojure "1.2.1"]
   					[com.cloudbees/cloudbees-api-client "1.0.0-SNAPSHOT"]
                     [lein-ring "0.4.5"]]	
